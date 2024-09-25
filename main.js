21b1a225ef360b1fee315beb3ac72501735abac2
AmazinAmazon <mati.bertrand@icloud.com>
2024-09-25 23:28:44 +0200

    fixed logic

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// logic: deck of cards of N cards divided in 2 people 
-
 function tirerLaSerie(n) {
     const cartesDuJeu = [];
     const used = new Set();
@@ -12,43 +10,31 @@ function tirerLaSerie(n) {
         }
     }
 
-    const cartes = arrangerCartes(cartesDuJeu);
-    const pOF = pileOuFace();
-    if (pOF == 1) { // Si pile, attribuer la premiere pile au joueur 1
-        document.getElementById("cartesJ1").textContent = cartes[0]
-        document.getElementById("cartesJ2").textContent = cartes[1]
-    } else {
-        document.getElementById("cartesJ1").textContent = cartes[1]
-        document.getElementById("cartesJ2").textContent = cartes[0]
-
-    }
+    cartes = arrangerCartes(cartesDuJeu);
+    document.getElementById("cartesJ1").textContent = cartes[0]
+    document.getElementById("cartesJ2").textContent = cartes[1]
 }
 
 function arrangerCartes(cartes) {
-    if (cartes.length % 2 == 0) { //si le nombres de cartes est pair
-        splitNbr = cartes.length / 2;
-        cartes1 = [];
-        cartes2 = [];
-        cartes1 = cartes.slice(0, splitNbr) //première moitié des cartes
-        cartes2 = cartes.slice(splitNbr, cartes.length) //deuxieme moitié des cartes
-
-    } else {
-        splitNbr = (cartes.length + 1) / 2;
-        cartes1 = [];
-        cartes2 = [];
-        cartes1 = cartes.slice(0, splitNbr) //première moitié des cartes
-        cartes2 = cartes.slice(splitNbr, cartes.length) //deuxieme moitié des cartes
-
+    var cartes1 = []
+    var cartes2 = []
+    console.log(cartes, cartes.length)
+    for (let i = 0; i < cartes.length; i++) {
+        const pOF = pileOuFace()
+        console.log(pOF)
+        if (pOF == 0) {
+            cartes1.push(cartes[i]);
+        } else {
+            cartes2.push(cartes[i]);
+        }
     }
-    cartesTXT = document.getElementById("cartesTXT");
+
+    const cartesTXT = document.getElementById("cartesTXT");
     cartesTXT.textContent = `Cartes1: [${cartes1}], Cartes2: [${cartes2}]`
     return [cartes1, cartes2];
 }
 
-// Joueur 1 = Pile = 1
-// Joueur 2 = Face = 0
 function pileOuFace() {
     const rand = Math.floor(Math.random() * 2);
-    console.log(rand)
     return rand;
 }
